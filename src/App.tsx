3202b067a12168fe9d4a46bab2129b6489e706df
Rafael Souza <rafael.souza@iexfy.com>
2024-03-10 11:44:44 -0300

    setup, configs and header

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Transactions } from "./pages/Transactions";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from "styled-components"
+
+export function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      
+      <Transactions/>
+    </ThemeProvider>
+  )
+}
+
